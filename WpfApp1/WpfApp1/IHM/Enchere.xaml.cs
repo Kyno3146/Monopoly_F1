fb3c57f6fac8a24decbfc0e9d9cb27bd4e9851be
Kyno3146 <kyllian_riviere@etu.u-bourgogne.fr>
2025-05-22 15:12:19 +0200

    Cache

---
+++ WpfApp1/WpfApp1/IHM/Enchere.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Monopoly.IHM
+{
+    /// <summary>
+    /// Logique d'interaction pour Enchere.xaml
+    /// </summary>
+    public partial class Enchere : Window
+    {
+        private List<string> info = new List<string>();
+        private string Joueur1;
+        private string Joueur2;
+
+        public Enchere(List<string> info, string Joueur1, string Joueur2)
+        {
+            InitializeComponent();
+            this.info = info;
+            this.Joueur1 = Joueur1;
+            infoLoad(info);
+        }
+
+        /// <summary>
+        /// Load the information of the card in the auction window.
+        /// </summary>
+        /// <param name="info"></param>
+        ///<author>Barthoux Sauze Thomas</author>
+        private void infoLoad(List<string> info)
+        {
+            this.LabelCarte.Content = info[1];
+            string imagePath = "Image/" + info[0] + ".png";
+            this.imgCarte.Source = new BitmapImage(new Uri(imagePath, UriKind.Relative));
+            this.description.Text = info[2];
+
+            // Par défaut, vider les champs
+            this.valachat.Text = "";
+
+
+            // Si valeur d'achat présente
+            if (info.Count > 3)
+                this.valachat.Text = info[3];
+
+            // Si total est présent (en dernière position)
+            if (info.Count > 5)
+                this.valachat.Text += "\n" + info[info.Count - 1];  // Affiche le total dans valachat
+
+            // Affichage des joueurs
+            this.Joueur1Label.Content = Joueur1;
+        }
+
+    }
+}
