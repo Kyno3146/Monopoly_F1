3ae15f23ae6738a12291d905faf51655b46d766b
Kyno3146 <kyllian_riviere@etu.u-bourgogne.fr>
2025-05-22 14:55:07 +0200

    cache

--- WpfApp1/WpfApp1/obj/Debug/net9.0-windows/Monopoly.AssemblyInfo.cs
+++ WpfApp1/WpfApp1/obj/Debug/net9.0-windows/Monopoly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Monopoly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd674ab729dbf33f99edf8e04ab7e74c4e69537a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e38ed1378d85f496bf408f55f3010177335d726")]
 [assembly: System.Reflection.AssemblyProductAttribute("Monopoly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Monopoly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
