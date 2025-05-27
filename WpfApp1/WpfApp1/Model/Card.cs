6b4a0ac849566d37491b1dee4543b6043e6b3d78
Père Castore <barthoux4@gmail.com>
2025-05-27 08:54:05 +0200

    Modifcation InfoCard
    Ajout d'un case pour chaque carte dans info carte que l'on puisse récupérer toute les valeur avec soit le nom ou sa position.

--- WpfApp1/WpfApp1/Model/Card.cs
+++ WpfApp1/WpfApp1/Model/Card.cs
@@ -66,6 +66,9 @@ public class Card {
             case "case_f1_radio1":
             case "case_f1_radio_team2":
             case "case_f1_radio_team3":
+            case "2":
+            case "17":
+            case "33":
                 info.Add(nom);
                 info.Add("Radio F1 - Événement");
                 this.description = "Une communication radio intervient. Votre ingénieur vous répond.";
@@ -73,6 +76,7 @@ public class Card {
                 break;
 
             case "case_Canada":
+            case "3":
                 info.Add(nom);
                 info.Add("Circuit du Canada");
                 this.description = "Circuit de Montréal";
@@ -84,6 +88,7 @@ public class Card {
                 break;
 
             case "case_formula_e":
+            case "5":
                 info.Add(nom);
                 info.Add("Formule E");
                 this.description = "Découvrez la compétition électrique";
@@ -95,6 +100,7 @@ public class Card {
                 break;
 
             case "case_licence":
+            case "4":
                 info.Add(nom);
                 info.Add("Licence Pilote");
                 this.description = "Vous obtenez une licence de course. Nécessaire pour certaines compétitions.";
@@ -104,6 +110,7 @@ public class Card {
                 break;
 
             case "case_imola":
+            case "6":
                 info.Add(nom);
                 info.Add("Circuit d'Imola");
                 this.description = "Circuit légendaire d'Italie";
@@ -115,6 +122,9 @@ public class Card {
                 break;
 
             case "case_fia":
+            case "7":
+            case "22":
+            case "36":
                 info.Add(nom);
                 info.Add("FIA - Événement");
                 this.description = "Visite à la FIA, événement spécial ou amende";
@@ -122,6 +132,7 @@ public class Card {
                 break;
 
             case "case_jeddah":
+            case "8":
                 info.Add(nom);
                 info.Add("Circuit de Jeddah");
                 this.description = "Course urbaine en Arabie Saoudite";
@@ -133,6 +144,7 @@ public class Card {
                 break;
 
             case "case_gpabu":
+            case "9":
                 info.Add(nom);
                 info.Add("Grand Prix d'Abu Dhabi");
                 this.description = "Dernière course de la saison";
@@ -144,6 +156,7 @@ public class Card {
                 break;
 
             case "case_raceban":
+            case "10":
                 info.Add(nom);
                 info.Add("Interdiction de Course");
                 this.description = "Vous êtes suspendu pour une course.";
@@ -151,6 +164,7 @@ public class Card {
                 break;
 
             case "case_azerbaijan":
+            case "11":
                 info.Add(nom);
                 info.Add("Circuit d'Azerbaïdjan");
                 this.description = "Course dans les rues de Bakou";
@@ -162,6 +176,7 @@ public class Card {
                 break;
 
             case "case_prost":
+            case "12":
                 info.Add(nom);
                 info.Add("Légende - Alain Prost");
                 this.description = "Vous rencontrez Alain Prost. Gagnez une carte bonus.";
@@ -173,6 +188,7 @@ public class Card {
                 break;
 
             case "case_austin":
+            case "13":
                 info.Add(nom);
                 info.Add("Circuit d'Austin");
                 this.description = "Course au Texas, USA";
@@ -184,6 +200,7 @@ public class Card {
                 break;
 
             case "case_mexico":
+            case "14":
                 info.Add(nom);
                 info.Add("Grand Prix du Mexique");
                 this.description = "Course en altitude, Mexico City";
@@ -195,6 +212,7 @@ public class Card {
                 break;
 
             case "case_gt_world":
+            case "15":
                 info.Add(nom);
                 info.Add("GT World");
                 this.description = "Championnat du monde GT, participez avec votre écurie.";
@@ -206,6 +224,7 @@ public class Card {
                 break;
 
             case "case_Pays_Bas":
+            case "16":
                 info.Add(nom);
                 info.Add("Circuit des Pays-Bas");
                 this.description = "Course à Zandvoort";
@@ -217,6 +236,7 @@ public class Card {
                 break;
 
             case "case_chine":
+            case "18":
                 info.Add(nom);
                 info.Add("Grand Prix de Chine");
                 this.description = "Course sur le circuit de Shanghai";
@@ -228,6 +248,7 @@ public class Card {
                 break;
 
             case "case_bahrain":
+            case "19":
                 info.Add(nom);
                 info.Add("Grand Prix du Bahreïn");
                 this.description = "Course nocturne dans le désert";
@@ -239,6 +260,7 @@ public class Card {
                 break;
 
             case "case_trevehivernal":
+            case "20":
                 info.Add(nom);
                 info.Add("Trêve Hivernale");
                 this.description = "Pause dans la saison. Aucun effet.";
@@ -246,6 +268,7 @@ public class Card {
                 break;
 
             case "case_vegas":
+            case "21":
                 info.Add(nom);
                 info.Add("Grand Prix de Las Vegas");
                 this.description = "Course spectaculaire sur le Strip";
@@ -257,6 +280,7 @@ public class Card {
                 break;
 
             case "case_australie":
+            case "23":
                 info.Add(nom);
                 info.Add("Grand Prix d'Australie");
                 this.description = "Course d'ouverture à Melbourne";
@@ -268,6 +292,7 @@ public class Card {
                 break;
 
             case "case_autriche":
+            case "24":
                 info.Add(nom);
                 info.Add("Grand Prix d'Autriche");
                 this.description = "Red Bull Ring";
@@ -279,6 +304,7 @@ public class Card {
                 break;
 
             case "case_wrc":
+            case "25":
                 info.Add(nom);
                 info.Add("Championnat WRC");
                 this.description = "Participer au rallye mondial";
@@ -290,6 +316,7 @@ public class Card {
                 break;
 
             case "case_singapour":
+            case "26":
                 info.Add(nom);
                 info.Add("Grand Prix de Singapour");
                 this.description = "Course nocturne dans les rues";
@@ -301,6 +328,7 @@ public class Card {
                 break;
 
             case "case_silverstone":
+            case "27":
                 info.Add(nom);
                 info.Add("Silverstone - Royaume-Uni");
                 this.description = "L'un des circuits les plus anciens";
@@ -312,6 +340,7 @@ public class Card {
                 break;
 
             case "case_Senna":
+            case "28":
                 info.Add(nom);
                 info.Add("Légende - Ayrton Senna");
                 this.description = "Hommage à Senna. Bonus de popularité.";
@@ -321,6 +350,7 @@ public class Card {
                 break;
 
             case "case_espagne":
+            case "29":
                 info.Add(nom);
                 info.Add("Grand Prix d'Espagne");
                 this.description = "Circuit de Barcelone";
@@ -332,6 +362,7 @@ public class Card {
                 break;
 
             case "case_commisaire":
+            case "30":
                 info.Add(nom);
                 info.Add("Bureau des Commissaires");
                 this.description = "Inspection ou pénalité possible.";
@@ -340,6 +371,7 @@ public class Card {
                 break;
 
             case "case_japon":
+            case "31":
                 info.Add(nom);
                 info.Add("Grand Prix du Japon");
                 this.description = "Circuit de Suzuka";
@@ -351,6 +383,7 @@ public class Card {
                 break;
 
             case "case_bresil":
+            case "32":
                 info.Add(nom);
                 info.Add("Grand Prix du Brésil");
                 this.description = "Course à Interlagos";
@@ -362,6 +395,7 @@ public class Card {
                 break;
 
             case "case_belgique":
+            case "34":
                 info.Add(nom);
                 info.Add("Grand Prix de Belgique");
                 this.description = "Spa-Francorchamps";
@@ -373,6 +407,7 @@ public class Card {
                 break;
 
             case "case_wec":
+            case "35":
                 info.Add(nom);
                 info.Add("Championnat WEC");
                 this.description = "World Endurance Championship";
@@ -384,6 +419,7 @@ public class Card {
                 break;
 
             case "case_monza":
+            case "37":
                 info.Add(nom);
                 info.Add("Circuit de Monza");
                 this.description = "Temple de la vitesse en Italie";
@@ -395,6 +431,7 @@ public class Card {
                 break;
 
             case "case_amende_FIA":
+            case "38":
                 info.Add(nom);
                 info.Add("Amende FIA");
                 this.description = "Vous recevez une amende. Payer 100 000€";
@@ -404,6 +441,7 @@ public class Card {
                 break;
 
             case "case_monaco":
+            case "39":
                 info.Add(nom);
                 info.Add("Grand Prix de Monaco");
                 this.description = "Course urbaine prestigieuse";
--- WpfApp1/WpfApp1/obj/Debug/net9.0-windows/IHM/Register.g.i.cs
+++ WpfApp1/WpfApp1/obj/Debug/net9.0-windows/IHM/Register.g.i.cs
@@ -69,7 +69,7 @@ namespace Monopoly.IHM {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Monopoly;V1.0.0.0;component/ihm/register.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Monopoly;component/ihm/register.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\IHM\Register.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
